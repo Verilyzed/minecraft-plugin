f29f8cdb701c0e2c56675d16343c0aa3e46e0d09
CNSCHNEI <colin.schneider@btc-ag.com>
2021-08-26 15:38:15 +0200

    thies

---
+++ src/main/java/de/verilyzed/krassalla/KrassAlla.java
@@ -0,0 +1,17 @@
+package de.verilyzed.krassalla;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KrassAlla extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
