150ededc5eef6b8ff36c2b587a8c2e660991c360
CNSCHNEI <colin.schneider@btc-ag.com>
2021-08-27 09:06:21 +0200

    thies

--- src/main/java/de/verilyzed/krassalla/KrassAlla.java
+++ src/main/java/de/verilyzed/krassalla/KrassAlla.java
@@ -4,6 +4,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class KrassAlla extends JavaPlugin {
 
+    public static String PREFIX = "§b§6[§2KrassAlla§b§6]§f ";
+
     @Override
     public void onEnable() {
         // Plugin startup logic
@@ -13,5 +15,9 @@ public final class KrassAlla extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public void log(String text) {
+        System.out.println(PREFIX + text);
     }
 }
