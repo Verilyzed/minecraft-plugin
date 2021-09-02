8c4cf29c906c325f57a00a40423c4424c47b5a5f
Thies Johann Brötje <thiesjohann.broetje@btc-ag.com>
2021-09-02 11:32:34 +0200

    Updated /money so you can give other people cass

--- src/main/java/de/verilyzed/commands/money.java
+++ src/main/java/de/verilyzed/commands/money.java
@@ -17,10 +17,7 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 public class money implements CommandExecutor {
@@ -32,7 +29,47 @@ public class money implements CommandExecutor {
             if (sender instanceof Player) {
                 Player p = (Player) sender;
                 if(args.length>0) {
+                    if (args[0].equalsIgnoreCase("give") && args.length == 3) {
+                        try {
+                            FileReader fr = new FileReader(KrassAlla.dataFolder + "\\PlayerData\\" + p.getUniqueId() + ".json");
+                            Scanner scanner  = new Scanner(fr);
+                            JSONParser parser = new JSONParser();
+                            JSONObject jsonObject = (JSONObject) parser.parse(scanner.nextLine());
+                            long money = (long)jsonObject.get("money");
+                            scanner.close();
+                            fr.close();
+                            String recuuid = "";
+                            if (money<Integer.parseInt(args[2])) {
+                                p.sendMessage(KrassAlla.PREFIX + "Du hast nicht genügend Geld.");
+                            }
+                            for (Player receiver : Bukkit.getOnlinePlayers()) {
+                                if(!args[1].equalsIgnoreCase(p.getName()))
+                                    continue;
+                                recuuid = receiver.getUniqueId().toString();
+                                break;
+                            }
+                            FileReader frreceiver = new FileReader(KrassAlla.dataFolder + "\\PlayerData\\" + recuuid + ".json");
+                            Scanner scannerreceiver  = new Scanner(frreceiver);
+                            JSONObject jsonObjectreceiver = (JSONObject) parser.parse(scanner.nextLine());
+                            long moneyreceiver = (long)jsonObjectreceiver.get("money");
+                            scannerreceiver.close();
+                            frreceiver.close();
+                            moneyreceiver += Integer.parseInt(args[2]);
+                            money -= Integer.parseInt(args[2]);
+                            jsonObjectreceiver.put("money", moneyreceiver);
+                            jsonObject.put("money", money);
+                            FileWriter fileWriter = new FileWriter(KrassAlla.dataFolder + "\\PlayerData\\" + p.getPlayer().getUniqueId() + ".json");
+                            fileWriter.write(jsonObject.toJSONString());
+                            fileWriter.close();
+                            fileWriter = new FileWriter(KrassAlla.dataFolder + "\\PlayerData\\" + recuuid + ".json");
+                            fileWriter.write(jsonObjectreceiver.toJSONString());
+                            fileWriter.close();
 
+                        } catch (IllegalStateException | ParseException | NumberFormatException | IOException e) {
+                            e.printStackTrace();
+                        }
+                        return true;
+                    }
                  return true;
                 }
                 try {
