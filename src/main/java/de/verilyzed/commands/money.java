4ec5494b501e45e9207626257f9229d85045e0dd
Thies Johann Brötje <thiesjohann.broetje@btc-ag.com>
2021-09-02 10:49:43 +0200

    Created /money command that prints the current balance to chat.

---
+++ src/main/java/de/verilyzed/commands/money.java
@@ -0,0 +1,56 @@
+package de.verilyzed.commands;
+
+import de.verilyzed.krassalla.KrassAlla;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class money implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+
+        if (command.getName().equalsIgnoreCase("money")) {
+            if (sender instanceof Player) {
+                Player p = (Player) sender;
+                if(args.length>0) {
+
+                 return true;
+                }
+                try {
+                    FileReader fr = new FileReader(KrassAlla.dataFolder + "\\PlayerData\\" + p.getUniqueId() + ".json");
+                    Scanner scanner  = new Scanner(fr);
+                    JSONParser parser = new JSONParser();
+                    JSONObject jsonObject = (JSONObject) parser.parse(scanner.nextLine());
+                    int money = (int)jsonObject.get("money");
+                    scanner.close();
+                    fr.close();
+                    p.sendMessage(KrassAlla.PREFIX + "Du hast " + money + "Geld");
+                } catch (IllegalStateException | ParseException | NumberFormatException | IOException e) {
+                    e.printStackTrace();
+                }
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
