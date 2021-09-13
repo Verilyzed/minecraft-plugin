f3e636b08da78875028d01432a777fdd0ca07d1d
Thies Johann Brötje <thiesjohann.broetje@btc-ag.com>
2021-09-13 13:20:05 +0200

    refactored money.java a bit

--- src/main/java/de/verilyzed/commands/money.java
+++ src/main/java/de/verilyzed/commands/money.java
@@ -10,10 +10,13 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.json.simple.JSONObject;
 
+import java.util.Objects;
 import java.util.UUID;
 
 public class money implements CommandExecutor {
 
+    // TODO: Generics kommen später
+    @SuppressWarnings("unchecked")
     private void setMoney(JSONObject p, long difference, UUID uuid) {
         p.put("money", difference);
         FileManager.setJSONObject(uuid, p);
@@ -22,49 +25,51 @@ public class money implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
 
-        if (command.getName().equalsIgnoreCase("money")) {
-            if (sender instanceof Player) {
-                //get the player, his uuid, his JSON and his money
-                Player p = (Player) sender;
-                UUID uuidSender = p.getUniqueId();
-                JSONObject senderJSON = FileManager.getJSONObject(uuidSender);
-                long moneySender = (long) senderJSON.get("money");
-                if (args.length > 0) {
-                    switch(args[0].toLowerCase()) {
-                        case "give":
-                            if (args.length <3) break;
-                            if (moneySender < Integer.parseInt(args[2]) || Integer.parseInt(args[2]) < 0) {
-                                p.sendMessage(KrassAlla.PREFIX + "Du hast nicht genügend Geld.");
-                                return true;
-                            }
-                            UUID uuidReceiver = Other.getPlayerUUID(args[1]);
-                            JSONObject receiverJSON = FileManager.getJSONObject(uuidReceiver);
-                            long moneyReceiver = (long) receiverJSON.get("money");
-                            moneyReceiver += Integer.parseInt(args[2]);
-                            moneySender -= Integer.parseInt(args[2]);
-                            setMoney(senderJSON, moneySender, uuidSender);
-                            setMoney(receiverJSON, moneyReceiver, uuidReceiver);
+        if (!command.getName().equalsIgnoreCase("money")) {
+            return false;
+        }
+
+        if (sender instanceof Player) {
+            //get the player, his uuid, his JSON and his money
+            Player p = (Player) sender;
+            UUID uuidSender = p.getUniqueId();
+            JSONObject senderJSON = FileManager.getJSONObject(uuidSender);
+            long moneySender = (long) Objects.requireNonNull(senderJSON).get("money");
+            if (args.length > 0) {
+                switch (args[0].toLowerCase()) {
+                    case "give":
+                        if (args.length < 3) break;
+                        if (moneySender < Integer.parseInt(args[2]) || Integer.parseInt(args[2]) < 0) {
+                            p.sendMessage(KrassAlla.PREFIX + "Du hast nicht genügend Geld.");
                             return true;
-                        case "add":
-                            if (args.length != 2)  {
-                                p.sendMessage("§sSyntax: /money add [Betrag]");
-                                return true;
-                            }
-                            moneySender += (long) Integer.parseInt(args[1]);
-                            setMoney(senderJSON, moneySender, uuidSender);
-                            p.sendMessage("Du Admin hast dir " + args[1] + " Eugen gegeben. Frech von dir.");
+                        }
+                        UUID uuidReceiver = Other.getPlayerUUID(args[1]);
+                        JSONObject receiverJSON = FileManager.getJSONObject(uuidReceiver);
+                        long moneyReceiver = (long) receiverJSON.get("money");
+                        moneyReceiver += Integer.parseInt(args[2]);
+                        moneySender -= Integer.parseInt(args[2]);
+                        setMoney(senderJSON, moneySender, uuidSender);
+                        setMoney(receiverJSON, moneyReceiver, uuidReceiver);
+                        return true;
+                    case "add":
+                        if (args.length != 2) {
+                            p.sendMessage("§sSyntax: /money add [Betrag]");
                             return true;
-                        default:
-                            p.sendMessage("§sFalscher Befehl, unterstützt wird:");
-                            p.sendMessage("/money");
-                            p.sendMessage("/money give [empfänger] [betrag]");
-                            p.sendMessage("/money add [betrag]");
-                            return true;
-                    }
+                        }
+                        moneySender += (long) Integer.parseInt(args[1]);
+                        setMoney(senderJSON, moneySender, uuidSender);
+                        p.sendMessage("Du Admin hast dir " + args[1] + " Eugen gegeben. Frech von dir.");
+                        return true;
+                    default:
+                        p.sendMessage("§sFalscher Befehl, unterstützt wird:");
+                        p.sendMessage("/money");
+                        p.sendMessage("/money give [empfänger] [betrag]");
+                        p.sendMessage("/money add [betrag]");
+                        return true;
                 }
-                p.sendMessage(KrassAlla.PREFIX + "Du hast " + moneySender + " Geld");
-                return true;
             }
+            p.sendMessage(KrassAlla.PREFIX + "Du hast " + moneySender + " Geld");
+            return true;
         }
         return false;
     }
