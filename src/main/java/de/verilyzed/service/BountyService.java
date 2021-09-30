85d1212ae252f5f3320a529099954f705f822606
CNSCHNEI <colin.schneider@btc-ag.com>
2021-09-30 14:06:36 +0200

    Bounty command erstellt, nicht vollständig Bounty class und repository erstellt

---
+++ src/main/java/de/verilyzed/service/BountyService.java
@@ -0,0 +1,20 @@
+package de.verilyzed.service;
+
+import de.verilyzed.persistence.model.Bounty;
+import de.verilyzed.persistence.repository.BountyRepository;
+
+public class BountyService {
+    private static BountyRepository bountyRepository;
+
+    public static void setBountyRepository(BountyRepository bountyRepository) {
+        BountyService.bountyRepository = bountyRepository;
+    }
+
+    public static void addBounty(Bounty bounty) {
+        bountyRepository.insertBounty(bounty);
+    }
+
+    public static void removeBountyByName(String name) {
+        bountyRepository.removeBounty(name);
+    }
+}
